f5112b4205796ddbbe79e5fe411bbac935f99da6
Angel_Escobar <angel.daniel.escobar77@gmail.com>
2023-12-11 04:08:24 -0400

    finally the page

---
+++ js.js
@@ -0,0 +1,7 @@
+
+const menu = document.getElementById("barra");
+const menucuadro = document.getElementById("menu")
+
+menu.addEventListener("click",() =>{
+    menucuadro.classList.toggle("menu--show")
+});
